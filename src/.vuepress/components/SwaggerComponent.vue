747b488705e743ce7296488f8dcdce41d64f0033
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-03-27 09:16:24 +0100

    Open API documentation

---
+++ src/.vuepress/components/SwaggerComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="swagger" id="swagger"></div>
+</template>
+
+<script>
+import SwaggerUI from 'swagger-ui';
+import 'swagger-ui/dist/swagger-ui.css';
+
+export default {
+  mounted() {
+    SwaggerUI({
+      url: 'https://v4.passwordless.dev/swagger/v4/swagger.json',
+      defaultModelsExpandDepth: -1,
+      showCommonExtensions: true,
+      showExtensions: true,
+      dom_id: '#swagger'
+    })
+  }
+}
+</script>
+
+<style>
+@import '../public/openapi.css';
+
+h2.title, .info__tos, .info__contact {
+  display: none;
+}
+</style>
